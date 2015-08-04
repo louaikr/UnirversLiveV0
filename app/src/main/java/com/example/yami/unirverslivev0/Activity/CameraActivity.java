86451c217fa53947f563b19ef9232f211ae24bb8
louaikr <louai.kraiem@gmail.com>
2015-08-04 14:47:01 +0100

    Merge branch 'master' of C:\Users\louai\AndroidStudioProjects\UnirversLiveV0 with conflicts.

--- app/src/main/java/com/example/yami/unirverslivev0/Activity/CameraActivity.java
+++ app/src/main/java/com/example/yami/unirverslivev0/Activity/CameraActivity.java
@@ -1,26 +1,18 @@
 package com.example.yami.unirverslivev0.Activity;
-
-
-import android.content.DialogInterface;
-
+import android.app.Activity;
 import android.hardware.Camera;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import com.example.yami.unirverslivev0.R;
 
 /**
  * Created by Yami on 7/31/2015.
  */
-public class CameraActivity extends AppCompatActivity {
-
+public class CameraActivity extends Activity {
+//test1n
     private Camera mCamera;
     private StreamPreview mPreview;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,34 +26,7 @@ public class CameraActivity extends AppCompatActivity {
         mPreview = new StreamPreview(this, mCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
-
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-
-        alert.setTitle("what's happening now");
-        alert.setMessage("Enter streaming title");
-
-// Set an EditText view to get user input
-        final EditText input = new EditText(this);
-        alert.setView(input);
-
-        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                String value = input.getText().toString();
-                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                // Canceled.
-            }
-        });
-
-        alert.show();
-
     }
-
     public static Camera getCameraInstance(){
         Camera c = null;
         try {
@@ -72,8 +37,4 @@ public class CameraActivity extends AppCompatActivity {
         }
         return c; // returns null if camera is unavailable
     }
-
-
-
-
 }
